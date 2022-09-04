f8e12e56f3b69b28dbe412bd843cee7e9d3a65c9
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2022-09-04 14:42:55 +0800

    serve

---
+++ README.md
@@ -0,0 +1,11 @@
+## server项目初始化
+
+```sh
+$ npm install ts-node -g
+$ npm init -y
+$ npm install @types/node -D
+$ npm install express -S
+$ npm install @types/express -D
+$ npm install axios -s
+```
+
---
+++ serve/index.ts
@@ -0,0 +1,28 @@
+import express, { Express, Router, Request, Response } from 'express';
+import axios from 'axios';
+
+const app: Express = express();
+
+// 配置路由
+const router: Router = express.Router({
+  // 在router 里面用到了 app.use 里面的动态参数，则需要加 mergeParams
+  mergeParams: true, //表示合并 url参数
+})
+
+app.use('/api', router)
+
+// json 格式接收
+// app.use(express.json())
+
+router.get('/list', async (req: Request, res: Response) => {
+  // req 接收前端传过来的东西
+  const result = await axios.post('https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=statisGradeCityDetail,diseaseh5Shelf')
+  // res 发送给前端
+  res.json({
+    data: result.data,
+  })
+})
+
+app.listen('3333', () => {
+  console.log('success server http://localhost:3333');
+})
